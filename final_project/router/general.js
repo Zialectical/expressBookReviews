2b85df03456fa7742ffef6459e8c42c6b7dd6d1c
Zialectical <chris@zialectical.com>
2024-07-21 15:41:27 -0700

    updated general.js to return neatly formatted json by removing 'stringify' command

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,8 +31,7 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    // Convert the books object to a JSON string with pretty printing
-    return res.status(200).json(JSON.stringify(books, null, 2));
+    return res.status(200).json(books); // Respond with the books object directly
 });
 
 // Get book details based on ISBN
@@ -71,28 +70,4 @@ public_users.get('/title/:title', function (req, res) {
     const booksByTitle = [];
 
     for (let isbn in books) {
-        if (books[isbn].title === title) {
-            booksByTitle.push({ isbn, ...books[isbn] });
-        }
-    }
-
-    if (booksByTitle.length > 0) {
-        return res.status(200).json(booksByTitle);
-    } else {
-        return res.status(404).json({ message: "No books found with this title" });
-    }
-});
-
-// Get book review
-public_users.get('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-
-    if (book) {
-        return res.status(200).json(book.reviews);
-    } else {
-        return res.status(404).json({ message: "Book not found" });
-    }
-});
-
-module.exports.general = public_users;
+        if (books[isbn].title ===​⬤
